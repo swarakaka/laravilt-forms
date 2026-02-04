e9b31fa7241373e9f55abb73023ee7655c44ac0d
Swara <swara09@gmail.com>
2026-02-04 21:10:01 +0100

    Add Kurdish translations (ckb) for form components

---
+++ lang/ckb/forms.php
@@ -0,0 +1,95 @@
+<?php
+
+return [
+    // Select Component
+    'select' => [
+        'placeholder' => 'هەڵبژێرە',
+        'loading_message' => 'بارکردنی بژاردەکان...',
+        'no_search_results_message' => 'هیچ ئەنجامێک نەدۆزرایەوە',
+        'search_prompt' => 'بۆ گەڕان دەست بە نووسین بکە...',
+        'searching_message' => 'گەڕان دەکرێت...',
+        'create_modal_title' => 'دروستکردنی بژاردەی نوێ',
+        'create_modal_description' => 'بژاردەیەکی نوێ بۆ ئەم خانەیە دروست بکە. دوای دروستکردن بە شێوەیەکی خۆکارانە هەڵدەبژێردرێت.',
+        'edit_modal_title' => 'دەستکاریکردنی بژاردە',
+        'edit_modal_description' => 'وردەکارییەکانی بژاردەکە نوێ بکەرەوە. گۆڕانکارییەکان دەستبەجێ ڕەنگ دەدەنەوە.',
+        'cancel_button_label' => 'هەڵوەشاندنەوە',
+        'create_button_label' => 'دروستکردن',
+        'creating_button_label' => 'دروست دەکرێت...',
+        'save_changes_button_label' => 'پاشەکەوتکردنی گۆڕانکارییەکان',
+        'saving_button_label' => 'پاشەکەوت دەکرێت...',
+        'scroll_for_more_label' => 'بۆ زیاتر سکڕۆڵ بکە',
+        'search_placeholder' => 'گەڕان...',
+        'try_adjusting_search_label' => 'هەوڵبدە گەڕانەکەت بگونجێنیت',
+    ],
+
+    // IconPicker Component
+    'icon_picker' => [
+        'placeholder' => 'ئایکۆنێک هەڵبژێرە',
+        'search_placeholder' => 'گەڕان بۆ ئایکۆنەکان...',
+        'no_icons_found' => 'هیچ ئایکۆنێک نەدۆزرایەوە',
+    ],
+
+    // ColorPicker Component
+    'color_picker' => [
+        'placeholder' => 'ڕەنگێک هەڵبژێرە',
+        'swatches' => 'نموونەی ڕەنگەکان',
+        'common_colors' => 'ڕەنگە باوەکان',
+    ],
+
+    // FileUpload Component
+    'file_upload' => [
+        'drag_drop' => 'پەڕگەکانت ڕابکێشە و دایبنێ یان',
+        'browse' => 'گەڕان',
+        'aspect_ratio' => 'ڕێژەی ڕەهەند:',
+        'rotate_left' => 'سووڕانەوە بۆ چەپ',
+        'rotate_right' => 'سووڕانەوە بۆ ڕاست',
+        'flip_horizontal' => 'هەڵگەڕانەوەی ئاسۆیی',
+        'flip_vertical' => 'هەڵگەڕانەوەی ستوونی',
+        'zoom_in' => 'گەورەکردن',
+        'zoom_out' => 'بچووککردنەوە',
+        'cancel' => 'هەڵوەشاندنەوە',
+        'reset' => 'ڕێکخستنەوە',
+        'save' => 'پاشەکەوتکردن',
+        'open_file' => 'کردنەوەی پەڕگە لە تابێکی نوێدا',
+        'download_file' => 'داگرتنی پەڕگە',
+    ],
+
+    // Repeater Component
+    'repeater' => [
+        'add_button_label' => 'زیادکردن',
+        'delete_button_label' => 'سڕینەوە',
+        'item_label' => 'دانە',
+        'items_label' => 'دانە',
+        'min_label' => 'کەمترین:',
+        'max_label' => 'زۆرترین:',
+    ],
+
+    // KeyValue Component
+    'key_value' => [
+        'key_label' => 'کلیل',
+        'value_label' => 'بەها',
+        'add_button_label' => 'زیادکردنی دانە',
+    ],
+
+    // Common
+    'common' => [
+        'required' => 'پێویستە',
+        'optional' => 'ئارەزوومەندانە',
+        'helper_text' => 'دەقی یارمەتیدەر',
+    ],
+
+    // Validation
+    'validation' => [
+        'required' => 'ئەم خانەیە پێویستە.',
+        'min_items' => 'تکایە لانیکەم :min دانە هەڵبژێرە',
+        'max_items' => 'تەنها دەتوانیت تا :max دانە هەڵبژێریت',
+    ],
+
+    // CheckboxList Component
+    'checkbox_list' => [
+        'select_all' => 'هەمووی دیاریبکە',
+        'select_all_permissions' => 'هەموو دەسەڵاتەکان دیاریبکە',
+        'selected' => 'دیاریکراو',
+        'search_placeholder' => 'گەڕان بۆ دەسەڵاتەکان...',
+    ],
+];
